fff88258b81e08f8fea0e8fccc29cb241a4e56fe
Shrey K <srkkadia@gmail.com>
2024-09-03 20:29:41 +0530

    Insert_At_End, Insert_At_Position, searchByValue

--- Linear DS/LinkedList/SinglyLL.c
+++ Linear DS/LinkedList/SinglyLL.c
@@ -6,15 +6,73 @@ struct Node {
     struct Node* next;
 };
 
-void insertAtBeginning(struct Node** head, int data) {    
+struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     if (newNode == NULL) {
         printf("Memory allocation failed!\n");
-        return;
+        return NULL;
     }
+    newNode->data = data;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void insertAtBeginning(struct Node** head, int data) {    
+    struct Node* newNode = createNode(data);
     newNode->data = data;
     newNode->next = *head;
     *head = newNode;
+}
+
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+
+    if(*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* last = *head;
+    while(last->next != NULL) last = last->next;
+    last->next = newNode;
+}
+
+void insertAtPosition(struct Node** head, int data, int position) {
+    if(position < 1) {
+        printf("Invalid Position.\n");
+        return;
+    }
+
+    struct Node* newNode = createNode(data);
+    if(position == 1) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    for (int i = 1; i < position - 1; i++) {
+        temp = temp->next;
+    }
+
+    if(temp == NULL) {
+        printf("Position is out of bounds.\n");
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+int searchByValue(struct Node* head, int key) {
+    while(head != NULL) {
+        if (head->data == key) {
+            return 1;
+        }
+        head = head->next;
+    }
+    return 0;
 }
 
 void printList(struct Node* head) {
@@ -26,6 +84,7 @@ void printList(struct Node* head) {
     printf("NULL\n");
 }
 
+// Input: 2 22 2 23 2 24 2 25 2 26 2 27 1 21
 int main() {
     int choice, data, position, key;
     struct Node* head = NULL;
@@ -56,25 +115,26 @@ int main() {
             insertAtBeginning(&head, data);
             break;
 
-        // case 2:
-        //     printf("\nEnter the data: ");
-        //     scanf("%d", &data);
-        //     InsertAtEnd(&head, data);
-        //     break;
+        case 2:
+            printf("\nEnter the data: ");
+            scanf("%d", &data);
+            insertAtEnd(&head, data);
+            break;
 
-        // case 3:
-        //     printf("\nEnter the position: ");
-        //     scanf("%d", &position);
-        //     printf("\nEnter the data: ");
-        //     scanf("%d", &data);
-        //     InsertAtPos(&head, data, position);
-        //     break;
+        case 3:
+            printf("\nEnter the position: ");
+            scanf("%d", &position);
+            printf("\nEnter the data: ");
+            scanf("%d", &data);
+            insertAtPosition(&head, data, position);
+            break;
 
-        // case 4:
-        //     printf("\nEnter the key to search from the list: ");
-        //     scanf("%d", &key);
-        //     searchByValue(head, key) ? printf("\n%d present in the list...", key) : printf("\n%d is not present in the list...", key);
-        //     break;
+        case 4:
+            printf("\nEnter the key to search from the list: ");
+            scanf("%d", &key);
+            searchByValue(head, key) ? printf("\n%d present in the list...\n", key) : printf("\n%d is not present in the list...\n", key);
+            system("pause");
+            break;
 
         // case 5:
         //     printf("\nLength of the list: %d\n", getLength(head));
